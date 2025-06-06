94defaf76092d7c9b85873d78bf782c54960df8b
Shane <6071159+smashedr@users.noreply.github.com>
2025-06-06 13:33:05 -0700

    Ensure Work is Disabled

--- app/src/main/java/org/cssnr/remotewallpaper/work/AppWorker.kt
+++ app/src/main/java/org/cssnr/remotewallpaper/work/AppWorker.kt
@@ -5,8 +5,10 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.preference.PreferenceManager
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import org.cssnr.remotewallpaper.MainActivity.Companion.LOG_TAG
 import org.cssnr.remotewallpaper.ui.home.updateWallpaper
 import org.cssnr.remotewallpaper.widget.WidgetProvider
 
@@ -15,6 +17,16 @@ class AppWorker(appContext: Context, workerParams: WorkerParameters) :
     override suspend fun doWork(): Result {
         Log.i("AppWorker", "START: doWork")
 
+        // Check Work Interval
+        val preferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val workInterval = preferences.getString("work_interval", null) ?: "0"
+        Log.d("AppWorker", "workInterval: $workInterval")
+        if (workInterval == "0") {
+            Log.i(LOG_TAG, "Work is Disabled.")
+            return Result.success()
+        }
+
+        // Update Wallpaper
         applicationContext.updateWallpaper()
 
         // Update Widget
