1e94ca1019570b828d49c36de9da337ac2bc2fc3
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2026-09-15 11:47:31 -0700

    Add Splash Screen (#67)

--- app/src/main/java/org/cssnr/remotewallpaper/MainActivity.kt
+++ app/src/main/java/org/cssnr/remotewallpaper/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import androidx.core.net.toUri
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -54,6 +55,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         Log.d(LOG_TAG, "onCreate: savedInstanceState: ${savedInstanceState?.size()}")
         enableEdgeToEdge(
